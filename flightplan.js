8774adee466335c4f942de37e59e4dc3ae81bab3
Samarth Shah <samarthmshah@gmail.com>
2015-10-01 23:05:23 -0400

    modify flightplan to segregate build and deploy tasks, makes it easier

--- flightplan.js
+++ flightplan.js
@@ -1,36 +1,55 @@
+'use strict';
+
 const plan = require('flightplan');
 
+const appName = 'samarth-xyz'
 const host = '108.167.189.59'
 const username = 'samarthmshah';
 const port = 2222
 
 // configuration
-plan.target('production', [
-  {
+plan.target('production', [{
     host: host,
     username: username,
     port: port,
     agent: process.env.SSH_AUTH_SOCK
-  },
-]);
+}, ]);
 
-var tmpDir = 'samarth-xyz-' + new Date().getTime();
+if (plan.runtime.target === 'production') {
+    var isReady = transport.prompt('Ready for deploying to production? [yes]');
+    if (isReady.indexOf('yes') === -1) {
+        plan.abort('User canceled the damn flight!');
+    }
+}
 
-// Take a Local flight
-plan.local(function(local) {
-  local.log('Copy files to remote hosts');
-  const filesToCopy = local.exec('git ls-files', {silent: true});
-  const destinationAddress = '/tmp/' + tmpDir;
-  // rsync files to all the target's remote hosts
-  local.transfer(filesToCopy, destinationAddress);
+const tmpDir = appName;
+
+plan.local('build', function(local) {
+    local.log('Build the dist/ directory');
+    local.exec('grunt build');
+    local.git('add .');
+    var commitMsg = local.prompt('What is the commit message?');
+    local.git('commit -m "' + commitMsg + '"');
+    var push = local.prompt('Do you also want to push it? [y/n]') === 'y';
+    if (push) {
+        local.git('push origin master');
+    }
+});
+
+plan.local('deploy', function(local) {
+    local.log('Copy files to remote hosts');
+    const filesToCopy = local.git('ls-files');
+    const destinationAddress = '/tmp/' + tmpDir;
+    // rsync files to all the target's remote hosts
+    local.transfer(filesToCopy, destinationAddress);
 });
 
 // run commands on the target's remote hosts
-plan.remote(function(remote) {
-  remote.log('Move folder to web root');
-  remote.rm('-rf ~/www/');  // remove old ones
-  remote.log('Removed previous www/ entries')
-  remote.exec('cp -a /tmp/'+tmpDir+'/dist/. ~/www/');  // This is having problems
-  remote.log('Copied the dist to ~/www/')
-  // remote.rm('-rf /tmp/' + tmpDir);
-  });
+plan.remote('copy', function(remote) {
+    remote.log('Move folder to web root. The User is ' + remote.hostname());
+    remote.rm('-rf ~/www/'); // remove old ones
+    remote.log('Removed previous www/ entries')
+    remote.cp('-a /tmp/' + tmpDir + '/dist/. ~/www/'); // This is having problems
+    remote.log('Copied the dist to ~/www/')
+        // remote.rm('-rf /tmp/' + tmpDir);
+});
